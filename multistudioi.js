d8435f508c23b663333362b8994bea53f2cd6acb
herdimasanggara <47119995+herdimasanggara@users.noreply.github.com>
2023-01-17 06:46:21 -0500

    Add files via upload

---
+++ multistudioi.js
@@ -0,0 +1,13 @@
+$(function(){
+
+$('.accordionDrop').hide();
+              $('.accordion').on('click', function(){
+			$(this).next().slideToggle("fast");
+		})
+
+              $('.accordionDrop').hide();
+              $('.accordionHighlight').on('click', function(){
+			$(this).next().slideToggle("fast");
+		})
+
+})
